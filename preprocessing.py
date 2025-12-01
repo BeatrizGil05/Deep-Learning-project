68d95fd0dce36b581ba69040910c925e7e51e814
BeatrizGil05 <beatrizcmgil@gmail.com>
2025-12-01 00:49:32 +0000

    ines + bia

--- preprocessing.py
+++ preprocessing.py
@@ -8,6 +8,12 @@ df = pd.read_csv('metadata.csv')
 # Clean metadata: Remove rows with missing file paths or family labels
 df = df.dropna(subset=['file_path', 'family']).reset_index(drop=True)
 
+# Duplicate rows in metadata
+duplicate_rows = df[df.duplicated()]
+print("Duplicate metadata rows:")
+print(duplicate_rows)
+df = df.drop_duplicates().reset_index(drop=True)
+
 # Adjust File Paths
 data_root_path = '/Users/jakubb/Desktop/rare_species' 
 df['full_path'] = df['file_path'].apply(lambda x: os.path.join(data_root_path, x))
@@ -23,8 +29,22 @@ if len(missing) > 0:
 # Drop rows with missing images
 df = df[df['exists'] == True].reset_index(drop=True)
 
+# Duplicate image paths
+duplicate_paths = df[df.duplicated(subset='full_path')]
+print("Duplicate file paths:")
+print(duplicate_paths)
+df = df.drop_duplicates(subset='full_path').reset_index(drop=True)
+
+# Encode each category in the target variable
+df['class_encoded'] = pd.factorize(df['family'])[0]
+unique_families = df['family'].unique()
+print(df['family'].nunique())
+df.head(3)
+
 # Check class imbalance
 print(df['family'].value_counts()) # para o report
+
+# Eploratory: Finding largest/smallest images
 
 # Stratified Split 
 # Split: 70% Train, 15% Validation, 15% Test
@@ -36,7 +56,3 @@ print(f"Train shape: {train_df.shape}")
 print(f"Val shape: {val_df.shape}")
 print(f"Test shape: {test_df.shape}")
 
-
-
-#from tensorflow.keras.preprocessing.image import ImageDataGenerator
-#from tensorflow.keras.regularizers import L1,L2
